a880007b44a5892eb3e8ee97f134c7fd3fd25a37
jvsobraz <jvsobraz@hotmail.com>
2023-04-18 10:36:47 -0300

    Implementação dos repositórios e arquivos

---
+++ src/main/java/br/com/fiap/digitalbusinessenablement/config/DatabaseSeeder.java
@@ -0,0 +1,5 @@
+package br.com.fiap.digitalbusinessenablement.config;
+
+public class DatabaseSeeder {
+    
+}
---
+++ src/main/java/br/com/fiap/digitalbusinessenablement/exceptions/RestNotFoundException.java
@@ -0,0 +1,5 @@
+package br.com.fiap.digitalbusinessenablement.exceptions;
+
+public class RestNotFoundException {
+    
+}

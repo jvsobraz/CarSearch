102e89a92b174cf775ef33af2c213a44e005f6e4
jvsobraz <jvsobraz@hotmail.com>
2023-05-05 11:26:16 -0300

    Update Despesa.java

--- src/main/java/br/com/fiap/digitalbusinessenablement/models/Despesa.java
+++ src/main/java/br/com/fiap/digitalbusinessenablement/models/Despesa.java
@@ -1,5 +1,60 @@
-package br.com.fiap.digitalbusinessenablement.models;
+package br.com.fiap.dindin.models;
 
-public class Despesa {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.EntityModel;
+
+import br.com.fiap.dindin.controllers.ContaController;
+import br.com.fiap.dindin.controllers.DespesaController;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Despesa{
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
+    @Min(value = 0, message = "deve ser positivo") 
+    @NotNull
+    private BigDecimal valor;
+   
+    @NotNull
+    private LocalDate data;
+
+    @NotBlank @Size(min = 5, max = 255)
+    private String descricao;
+
+    @ManyToOne
+    private Conta conta;
+
+    public EntityModel<Despesa> toEntityModel(){
+        return EntityModel.of(
+            this,
+            linkTo(methodOn(DespesaController.class).show(id)).withSelfRel(),
+            linkTo(methodOn(DespesaController.class).destroy(id)).withRel("delete"),
+            linkTo(methodOn(DespesaController.class).index(null, Pageable.unpaged())).withRel("all"),
+            linkTo(methodOn(ContaController.class).show(this.getConta().getId())).withRel("conta")
+        );
+    }
+
 }

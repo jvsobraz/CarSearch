de1ba3739efdee762b46780c26693fe49fa6e71b
jvsobraz <jvsobraz@hotmail.com>
2023-05-05 11:28:34 -0300

    Update DespesaRepository.java

--- src/main/java/br/com/fiap/digitalbusinessenablement/repository/DespesaRepository.java
+++ src/main/java/br/com/fiap/digitalbusinessenablement/repository/DespesaRepository.java
@@ -1,5 +1,17 @@
-package br.com.fiap.digitalbusinessenablement.repository;
+package br.com.fiap.dindin.repository;
 
-public class DespesaRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fiap.dindin.models.Despesa;
+
+public interface DespesaRepository extends JpaRepository<Despesa, Long> {
+
+    // @Query("SELECT d FROM Despesa d WHERE d.descricao LIKE %?1%") //JPQL
+    Page<Despesa> findByDescricaoContaining(String busca, Pageable pageable);
+
+    // @Query("SELECT d FROM Despesa d ORDER BY d.id LIMIT ?1 OFFSET ?2")
+    // List<Despesa> buscarPaginado(int tamanho, int offset);
     
 }

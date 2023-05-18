83ae14d90c7ddd00d55669d62483a4551b3131f6
jvsobraz <jvsobraz@hotmail.com>
2023-05-18 10:59:09 -0300

    Update AuthorizationFilter.java

--- src/main/java/br/com/fiap/digitalbusinessenablement/config/AuthorizationFilter.java
+++ src/main/java/br/com/fiap/digitalbusinessenablement/config/AuthorizationFilter.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import br.com.fiap.dindin.service.TokenJwtService;
+import br.com.fiap.digitalbusinessenablement.service.TokenJwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
